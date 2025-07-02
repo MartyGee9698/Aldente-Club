718c165f73786c39054813f7583e04d454aa2935
filippoago22 <filippo.agosti.14-2@accademiadigitaleliguria.it>
2025-07-02 17:40:39 +0200

    Commit per Merge

--- Aldente-Club/Aldente-Club/App.xaml.cs
+++ Aldente-Club/Aldente-Club/App.xaml.cs
@@ -4,11 +4,6 @@ namespace Aldente_Club
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            HomeWindow home = new HomeWindow();
-            home.Show();
-        }
+        
     }
 }
